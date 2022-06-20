001d7452d0920f2b2d08078b613fde8eb3d734de
Panthr75 <jjireland@stu.naperville203.org>
2022-06-20 18:51:50 -0500

    Add MinMaxRestriction

---
+++ AchievementsAPI/Utilities/MinMaxRestriction.cs
@@ -0,0 +1,30 @@
+﻿namespace AchievementsAPI.Utilities
+{
+    /// <summary>
+    /// Restrictions for a min max value.
+    /// </summary>
+    /// <typeparam name="T">The type of value.</typeparam>
+    public struct MinMaxRestriction<T>
+        where T : struct
+    {
+        /// <summary>
+        /// The minimum value
+        /// </summary>
+        public T Min { get; set; }
+        /// <summary>
+        /// The maximum value.
+        /// </summary>
+        public T Max { get; set; }
+
+        /// <summary>
+        /// Initializes this restriction with the provided min max values.
+        /// </summary>
+        /// <param name="min">The minimum value</param>
+        /// <param name="max">The maximum value</param>
+        public MinMaxRestriction(T min, T max)
+        {
+            this.Min = min;
+            this.Max = max;
+        }
+    }
+}
